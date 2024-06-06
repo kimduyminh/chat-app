4f152d58ea8deee62f6185b8d13b36c7eee9820a
aeMinh <aekim.meow@gmail.com>
2024-06-07 00:29:55 +0700

    messagecontroller and chatroomcontroller

--- src/main/java/com/project1/chatapp/chatroom/chatroomController.java
+++ src/main/java/com/project1/chatapp/chatroom/chatroomController.java
@@ -1,12 +1,31 @@
 package com.project1.chatapp.chatroom;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.project1.chatapp.message.messageService;
+
+import java.util.List;
 
 @RestController
 public class chatroomController {
     @Autowired
     private chatroomService chatroomService;
-
+    @Autowired
+    private messageService messageService;
+    @PostMapping("/app/{session_id}/createChatroom")
+    public ResponseEntity<String> createChatroom(@PathVariable String session_id,String name) {
+        chatroomService.createChatRoom(session_id,name);
+        return ResponseEntity.ok("Chatroom created");
+    }
+    @GetMapping("/app/{session_id}/loadchat")
+    public List<chatroomService.chatroomInfo> loadChat(@PathVariable String session_id) {
+        return chatroomService.listChatRoom(session_id);
+    }
+    @PostMapping("/app/{session_id}/{chatid}/delete")
+    
 }
---
+++ src/main/java/com/project1/chatapp/message/messageController.java
@@ -0,0 +1,35 @@
+package com.project1.chatapp.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.project1.chatapp.sessions.sessionService;
+
+import java.util.List;
+
+@Controller
+public class messageController {
+    @Autowired
+    private messageService messageService;
+    @Autowired
+    private sessionService sessionService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public messageController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/app/{session_id}/{chat_id}/sendm")
+    public void newMessage(@Payload message message,String chat_id,String session_id) {
+        messageService.newMessage(message,chat_id,session_id);
+        messagingTemplate.convertAndSend("/mainchat/{chat_id}",message);
+    }
+    @SubscribeMapping("/app/{session_id}/{chat_id}/loadm")
+    public List<message> listMessages(@PathVariable("session_id") String session_id, @PathVariable("chat_id")String chat_id) {
+        return messageService.listMessages(session_id,chat_id);
+    }
+}
